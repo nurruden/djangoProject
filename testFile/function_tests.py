7ab2bde5ee432ef13a1ce6ca3e90b323f00e7e85
nurruden <yangyanzhao@outlook.com>
2017-09-29 14:58:21 +0800

    Front page html can be generated from template

--- testFile/function_tests.py
+++ testFile/function_tests.py
@@ -4,7 +4,7 @@ __author__ = "Allan"
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
-
+import time
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -18,11 +18,15 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do',header_text)
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),'Enter a to-do item')
-        inputbox.send_keys('Buy peacock fea=thers')
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1:Buy peacock feathers' for row in rows))
+        time.sleep(10)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1:Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in tables"
+        )
         self.fail('Finish the test!')
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
